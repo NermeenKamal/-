d504640c2bd393ea17b8d05d540c935d16feb102
Nermeen Kamal <114883845+NermeenKamal@users.noreply.github.com>
2024-09-09 18:09:08 +0300

    Update content.php

--- content.php
+++ content.php
@@ -4,28 +4,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $recommendation = $_POST['recommendation'];
 
-    $serverName = "NERMEEN";
+    $serverName = "NERMEEN"; 
     $connectionOptions = array(
         "Database" => "recommend",
-        "TrustServerCertificate" => true
+        "TrustServerCertificate" => true,
+        "CharacterSet" => "UTF-8" 
     );
 
-    $serverName = "NERMEEN";
     $conn = sqlsrv_connect($serverName, $connectionOptions);
 
     if ($conn === false) {
         die("Error connecting to the database: " . print_r(sqlsrv_errors(), true));
     }
 
-
     $sql = "INSERT INTO Recommendations (name, email, recommendation) VALUES (?, ?, ?)";
     $params = array($name, $email, $recommendation);
+
     $stmt = sqlsrv_query($conn, $sql, $params);
 
     if ($stmt === false) {
         echo "Error inserting data: " . print_r(sqlsrv_errors(), true);
     } else {
-        echo "Success";
+        echo "Data inserted successfully!";
     }
 
     sqlsrv_close($conn);
